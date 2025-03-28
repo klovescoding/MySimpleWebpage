3d4fefaaace512220beb2d2e57b69ad337e98252
klovescoding <47025707+klovescoding@users.noreply.github.com>
2025-03-28 00:49:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -68,26 +68,6 @@ messageForm.addEventListener('submit', function (event) {
 
 
 
-/*   function fetchData(url) {
-      return fetch(url)
-      .then(res => res.json())
-      .catch(error => document.getElementById("projects").innerText = `${error}
 
-      Oh no - Looks like there's a problem loading the PROJECTS section...oops!!!`)
-   }
-   fetchData ("https://api.github.com/users/klovescoding/repos")
-   .then(function repo(data) {
-   
-    for (let i = 0; i < data.length; i++)  {
-   
-       let projectSection = document.getElementById("projects")
-       let projectList = projectSection.querySelector("ul");
-       let project = document.createElement("li")
-       let repoName = JSON.stringify(data[i].name)
-       project.innerText = repoName
-       projectList.appendChild(project);
-    }
-   })
-   */
 
 
